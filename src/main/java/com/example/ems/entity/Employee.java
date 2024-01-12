29aff3216d9bbd5edc4e3c61f3160e5869e32842
raiwat7 <101309747+raiwat7@users.noreply.github.com>
2024-01-12 19:37:42 +0530

    Initial Commit

---
+++ src/main/java/com/example/ems/entity/Employee.java
@@ -0,0 +1,31 @@
+package com.example.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    // by default, column name will be email
+    @Column(nullable = false, unique = true)
+    private String email;
+}
---
+++ src/main/java/com/example/ems/service/impl/EmployeeServiceImplementation.java
@@ -0,0 +1,24 @@
+package com.example.ems.service.impl;
+
+import com.example.ems.dto.EmployeeDto;
+import com.example.ems.entity.Employee;
+import com.example.ems.mapper.EmployeeMapper;
+import com.example.ems.repository.EmployeeRepository;
+import com.example.ems.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImplementation implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+}
